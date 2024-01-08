ffb1919d233d5a071116b0d80e9fa33fdbcf41d4
ethereumvd <153703868+ethereumvd@users.noreply.github.com>
2024-01-08 12:46:13 +0530

    Update Diffie_Hellmann_key_exchange.py

--- Diffie_Hellmann_key_exchange.py
+++ Diffie_Hellmann_key_exchange.py
@@ -1,30 +1,52 @@
 import random
-def generate_key(p, g, private_key):
-    public_key = pow(g, private_key, p)
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.backends import default_backend
+
+
+def generate_key(prime, primitive_root, private_key):
+    public_key = pow(primitive_root, private_key, prime)
     return public_key
 
-def calculate_shared_secret(public_key, private_key, p):
-    shared_secret = pow(public_key, private_key, p)
+def calculate_shared_secret(public_key, private_key, prime):
+    shared_secret = pow(public_key, private_key, prime)
     return shared_secret
 
-def main():
-    p = 23  # Usually a much larger prime number will be used
-    g = 5   # The common primitive root of p
-    
-    private_key_alice = random.randint(2, p - 2)
+def generate_prime_and_primitive_root(bits):
 
-    private_key_bob = random.randint(2, p - 2)
+    parameters = dh.generate_parameters(generator=5, key_size=bits, backend=default_backend())
+    prime = parameters.parameter_numbers().p
 
-    public_key_alice = generate_key(p, g, private_key_alice)
-    public_key_bob = generate_key(p, g, private_key_bob)
+    primitive_root = find_primitive_root(prime)
 
-    shared_secret_alice = calculate_shared_secret(public_key_bob, private_key_alice, p)
-    shared_secret_bob = calculate_shared_secret(public_key_alice, private_key_bob, p)
+    return prime, primitive_root
 
-    print("Public key of Alice:", public_key_alice)
-    print("Public key of Bob:", public_key_bob)
-    print("Shared secret for Alice:", shared_secret_alice)
-    print("Shared secret for Bob:", shared_secret_bob)
+def find_primitive_root(prime):
+    for candidate in range(2, prime - 1):
+        if pow(candidate, (prime - 1) // 2, prime) != 1 and pow(candidate, prime - 1, prime) == 1:
+            return candidate
 
-if __name__ == "__main__":
-    main()
+    raise ValueError("Primitive root not found")
+
+bit_length = 1024
+
+prime, primitive_root = generate_prime_and_primitive_root(bit_length)
+
+print(f"Generated",bit_length,f"bit prime: {prime}")
+print(f"Primitive root: {primitive_root}")
+
+private_key_alice = random.randint(2, prime)
+
+private_key_bob = random.randint(2, prime)
+
+public_key_alice = generate_key(prime, primitive_root, private_key_alice)
+
+public_key_bob = generate_key(prime, primitive_root, private_key_bob)
+
+shared_secret_alice = calculate_shared_secret(public_key_bob, private_key_alice, prime)
+
+shared_secret_bob = calculate_shared_secret(public_key_alice, private_key_bob, prime)
+
+print("Public key of Alice:", public_key_alice)
+print("Public key of Bob:", public_key_bob)
+print("Shared secret for Alice:", shared_secret_alice)
+print("Shared secret for Bob:", shared_secret_bob)
